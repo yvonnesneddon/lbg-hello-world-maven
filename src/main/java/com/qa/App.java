cfa5840c5d3813a414fc9238623909d06159bc8a
Yvonne Sneddon <Yvonne Sneddon>
2024-09-10 16:23:02 +0100

    name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
+        System.out.println(sayHelloToSomeone("Yvonne"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
